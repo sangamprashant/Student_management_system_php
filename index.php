df7d9a076ea9b554e2bf6801d83d0dbcccbd50a8
Prashant Srivastav <srivastavp891@gmail.com>
2023-07-26 02:38:00 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <nav>
+        <label class="logo">W-School</label>
+        <ul>
+            <li><a href=" ">Home</a></li>
+            <li><a href=" ">Contact</a></li>
+            <li><a href=" ">Admission</a></li>
+            <li><a href=" " class="btn btn-success">Login</a></li>
+
+        </ul>
+    </nav>
+    <div class="section1">
+        <label class="image_text">We Teach Student With Care</label>
+        <img class="main_img" src="img/school_management.jpg" />
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="welcome_img" src="img/school2.jpg" />
+            </div>
+            <div class="col-md-8">
+                <h1>Welcome To W-School</h1>
+                <p> jfvb3ffv 3 ff 3r uf rv v g b t iu ghr h duhg uigdff hdff dfuh di duhi gdh g higuh guhgdghdfhuh duhf kdhf hkdf hhk hklk f uh h h rhig hr rhe ehhk fh ehf 1erg er uherfg hklerkh er jhrf hgr hkrtjhg jhdf 1dffjbh dfjhdf fj b fg f mnf nf n f g mng mnfg mn mn mn f fg ng mng mn g </p>
+            </div>
+
+        </div>
+
+    </div>
+    <center>
+        <h1>Our Teachers</h1>
+    </center>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="img/teacher1.jpg" />
+                <p>in a Vibrant, academically challenging, and encouraging environment where manifold view points are prized and celebrtated</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="img/teacher2.jpg" />
+                <p>in a Vibrant, academically challenging, and encouraging environment where manifold view points are prized and celebrtated</p>
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="img/teacher3.jpg" />
+                <p>in a Vibrant, academically challenging, and encouraging environment where manifold view points are prized and celebrtated</p>
+            </div>
+        </div>
+    </div>
+    <center>
+        <h1>Our Courses</h1>
+    </center>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="teacher" src="img/graphic.jpg" />
+                <h3>Graphic Design</h3>
+
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="img//web.jpg" />
+                <h3>Web Development</h3>
+
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="img/marketing.png" />\
+                <h3>Marketing</h3>
+
+            </div>
+        </div>
+    </div>
+    <center >
+        <h1 class="adm">Admission Form</h1>
+    </center>
+    <div align="center" class="admission_form">
+        <form>
+            <div class="adm_int">
+                <label class="label_text">Name</label>
+                <input class="input_deg" type="text" name="" />
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Email</label>
+                <input class="input_deg" type="email" name="" />
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Phone</label>
+                <input class="input_deg" type="text" name="" />
+            </div>
+            <div class="adm_int">
+                <label class="label_text">Message</label>
+                <textarea class="int_txt"></textarea>
+            </div>
+            <div class="adm_int">
+                <input class="btn btn-primary" type="submit" id="submit" value="Apply" />
+            </div>
+        </form>
+    </div>
+    <footer>
+        <h3 class="footer_txt">All @copyright reserved by:Prashant Srivastav</h3>
+
+    </footer>
+
+
+</body>
+
+</html>
